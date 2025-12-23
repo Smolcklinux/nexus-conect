ddae51c4169265fff2ead26b4ddc2fdbb10b9fc4
Smolcklinux <gabrieln0661@gmail.com>
2025-12-23 03:47:03 -0300

    update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * NEXUS BRIDGE - Ponto de Entrada Principal
+ * Este arquivo conecta o Index ao motor na pasta /js
+ */
+
+// Importa o arquivo principal da pasta js
+// Como é um módulo, o navegador buscará todas as dependências (config.js, etc) automaticamente
+import './js/main.js';
+
+console.log("%c 🟢 NEXUS BRIDGE: Conectado ao motor interno.", "color: #00ff00; font-weight: bold;");
